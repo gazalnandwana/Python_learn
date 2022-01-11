c14b6338179bdf7ff0ebc52328099f8e7424bb3c
Gazal Nandwana <gazal0922@gmail.com>
2022-01-11 17:09:52 -0500

    Adding now file

---
+++ Homework/4.Simple programs/convertor.py
@@ -0,0 +1,6 @@
+minutes = int(input('Enter the number of minutes: '))
+seconds = int(input('Enter the number of seconds: '))
+hours = minutes/60
+count_min = seconds/60
+print(f'The number of hours are: {hours}')
+print(f'The number of minutes are: {count_min}')
