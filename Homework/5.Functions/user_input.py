a9d4d30319a9168fc0c5c87509fb33bb0a0ffc59
Gazal Nandwana <gazal0922@gmail.com>
2022-01-12 15:15:43 -0500

    Adding new folder

---
+++ Homework/5.Functions/user_input.py
@@ -0,0 +1,7 @@
+def add(x,y):
+  z= x + y
+  return print(z)
+
+first_number = int(input('Enter first number: '))
+second_number = int(input('Enter second number: '))
+add(first_number, second_number)
